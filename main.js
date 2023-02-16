db43e62d7ca15e78e3b51780f1867ea8aee737f7
vcosta5 <93163720+vcosta5@users.noreply.github.com>
2023-02-16 03:52:48 -0300

    Link-Tree mobile
    A versão inicial da linktree e do projeto.

---
+++ main.js
@@ -0,0 +1,5 @@
+function ExibirAlert(){
+    alert("Ainda não tá pronto meu patrão :(( \nMe da mais uns dias ai que isso aqui vai ficar gostosinho \nA proposito, seu pc acabou de ser infectado com meu trojan");    
+}
+
+document.querySelector('.construcao');
